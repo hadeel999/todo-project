649f139a8f43a601a52aa683bc2c82a02ae580ed
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-04-19 15:39:46 +0300

    Add todo-js2 branch

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ var gender=prompt("Please enter your gender.");
 while(!(gender.toLocaleLowerCase()==="male"||gender.toLocaleLowerCase()==="female" )){
     gender=prompt("Please enter your gender as: Male or Female.");
 }
-var age =prompt("Please enter your age.")
+var age =prompt("Please enter your age.");
 while(age<=0){
-    alert("You entered an age less than or equal zero.")
-    age =prompt("Please enter your age.")
+    alert("You entered an age less than or equal zero.");
+    age =prompt("Please enter your age.");
 }
 var skipWelcomePage =confirm("Would you like to skip the welcoming page?");
 (!skipWelcomePage&&gender.toLocaleLowerCase()==="male")?alert("Welcome Mr. "+userName):
 (!skipWelcomePage&&gender.toLocaleLowerCase()==="female")?alert("Welcome Ms. "+userName):
 (!skipWelcomePage)?alert("Welcome."):
 console.log("Welcome page is skipped!");
+
+let questions=["Did you eat your breakfast?","Do you like eating meat?","Do you like eating Knafeh"];
+let answers=[];
+let answer,valid;
+let askUser = (question) => {return prompt(question)}
+
+for(let i=0;i<3;i++){
+   answer=askUser(questions[i]);
+   (answer=="")?answers.push("invalid"):
+   answers.push(answer);
+   console.log(answers[i]);
+}
+
