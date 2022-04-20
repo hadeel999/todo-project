11812ce57dad95da5ef9e04b7467afa3036ebf30
Hadeel Saleh <103508563+hadeel999@users.noreply.github.com>
2022-04-20 13:36:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ console.log("Welcome page is skipped!");
 
 let questions=["Did you eat your breakfast?","Do you like eating meat?","Do you like eating Knafeh"];
 let answers=[];
+let answer,valid;
 let askUser = (question) => {return prompt(question)}
 let getAnswers = () => { 
     let answer;
