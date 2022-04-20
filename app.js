7864379144ea9c25c51fec83382d3ab12b280b20
Hadeel Saleh <103508563+hadeel999@users.noreply.github.com>
2022-04-20 13:29:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ console.log("Welcome page is skipped!");
 
 let questions=["Did you eat your breakfast?","Do you like eating meat?","Do you like eating Knafeh"];
 let answers=[];
-let answer,valid;
 let askUser = (question) => {return prompt(question)}
-
-for(let i=0;i<3;i++){
-   answer=askUser(questions[i]);
-   (answer=="")?answers.push("invalid"):
-   answers.push(answer);
-   console.log(answers[i]);
+let getAnswers = () => { 
+    let answer;
+    for(let i=0;i<3;i++){
+        answer=askUser(questions[i]);
+        (answer=="")?answers.push("invalid"):
+        answers.push(answer);
+        console.log(answers[i]);
+     }
 }
+
+getAnswers();
+
